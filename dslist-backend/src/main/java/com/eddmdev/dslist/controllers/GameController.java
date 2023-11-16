9d507bd9b72779e7acaac8aad646fbeb8e721487
Admilson Ferreira <ferreiraad3@gmail.com>
2023-11-16 17:36:45 -0300

    Game repository, service, controller

---
+++ dslist-backend/src/main/java/com/eddmdev/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.eddmdev.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eddmdev.dslist.dto.GameMinDTO;
+import com.eddmdev.dslist.entities.Game;
+import com.eddmdev.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+}
